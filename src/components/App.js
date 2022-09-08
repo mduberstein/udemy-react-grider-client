1e6123e39794715eff132f3c0b9804c71320dc7e
Michael Duberstein <dubermic@flutterint.com>
2022-09-08 18:33:40 -0400

    InClip 296 [Optional] - Different Router Types, HashRouter

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {BrowserRouter, Route, Link} from 'react-router-dom';
+import {HashRouter, Route, Link} from 'react-router-dom';
 
 const PageOne = () => {
   return (
@@ -23,10 +23,10 @@ const PageTwo = () => {
 const App = () => {
   return (
     <div>
-      <BrowserRouter>
+      <HashRouter>
           <Route path="/" exact component={PageOne} />
           <Route path="/pagetwo" component={PageTwo} />
-      </BrowserRouter>
+      </HashRouter>
     </div>
   );
 }
