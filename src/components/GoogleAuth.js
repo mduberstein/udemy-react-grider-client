24a138b9358675c83db0cfef49bfba066ff8528f
Michael Duberstein <dubermic@flutterint.com>
2022-09-10 20:28:40 -0400

    After Clip 307 Wiring Up the Google API Library

---
+++ src/components/GoogleAuth.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+class GoogleAuth extends React.Component {
+  componentDidMount() {
+    window.gapi.load('client:auth2', () => {
+      window.gapi.client.init({
+        clientId: '29079199241-nn46932v8jjqp62g8isp8nq90i74fmjq.apps.googleusercontent.com',
+        scope: 'email'
+      })
+    });
+  }
+
+  render() {
+    return <div>Google Auth</div>;
+  }
+}
+
+export default GoogleAuth;
