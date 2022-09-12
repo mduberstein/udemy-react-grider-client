7136eb2eb8b89b3ae67ca7fe1f241b4b35259295
Michael Duberstein <dubermic@flutterint.com>
2022-09-12 16:13:36 -0400

    After Clip 321 Using Redux Dev Tools to Inspect the Store

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
-import { createStore } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
 
 import App from "./components/App";
 import reducers from "./reducers";
 
-const store = createStore(reducers);
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__  || compose;
+// apply Middleware can be called without passing any arguments without causing an error
+// middleware arguments can be added later
+// see https://github.com/reduxjs/redux-devtools/tree/main/extension, Advanced Setup
+const store = createStore(reducers, composeEnhancers(applyMiddleware()));
 
 ReactDOM.render(
   <Provider store={store}>
