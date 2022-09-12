7d01455696cbef5851ce2f91be75c7d912267b8e
Michael Duberstein <dubermic@flutterint.com>
2022-09-12 18:56:56 -0400

    After Clip 326 Connecting Redux Form

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,9 @@
 import {combineReducers} from 'redux';
+import {reducer as formReducer } from 'redux-form'
 import authReducer from './authReducer';
 
 export default combineReducers ({
-  auth: authReducer
+  auth: authReducer,
+  // form is the precise name of the key required by redux-form lib
+  form: formReducer
 });
