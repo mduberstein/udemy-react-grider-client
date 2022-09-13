a503c9b7dc17373f83d53353264601ed1ec1da6b
Michael Duberstein <dubermic@flutterint.com>
2022-09-13 15:48:08 -0400

    In Clip 328 Automatically Handling Events, before using Redux Form shortcut syntax

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -2,8 +2,14 @@ import React from "react";
 import { Field, reduxForm } from "redux-form";
 
 class StreamCreate extends React.Component {
-  renderInput() {
-    return <div>I am an input!</div>;
+  renderInput(formProps) {
+    console.log("form input props:", formProps);
+    return (
+      <input 
+        onChange={formProps.input.onChange}
+        value={formProps.input.value}
+      />
+    );
   }
   render() {
     console.log("Props introduced with reduxForm", this.props);
