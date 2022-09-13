77d6868f384f0a07997c82550832f128812d8bc2
Michael Duberstein <dubermic@flutterint.com>
2022-09-13 16:36:26 -0400

    After Clip 330 Handling Form Submission

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -11,12 +11,25 @@ class StreamCreate extends React.Component {
       </div>
     );
   }
+
+  // typical on Submit, before the Redux Form lib
+  // onSubmit(event) {
+  //   event.preventDefault();
+  // }
+  onSubmit(formValues) {
+    console.log("formValues in onSubmit", formValues);
+  }
+
   render() {
     console.log("Props introduced with reduxForm", this.props);
     return (
-      <form className="ui form">
+      // handleSubmit calls event.preventDefault(), so our onSubmit doesn't have to
+      // also the onSubmit is called not with the event arguent, 
+      // but with values of the form's inputs
+      <form onSubmit={this.props.handleSubmit(this.onSubmit)} className="ui form">
         <Field name="title" component={this.renderInput} label="Enter Title"/>
         <Field name="description" component={this.renderInput} label="Enter Description"/>
+        <button className="ui button primary">Submit</button>
       </form>
     );
   }
