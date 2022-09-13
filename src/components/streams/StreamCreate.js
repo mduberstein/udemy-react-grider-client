88114894fa05a8f2ca8e8da996f220eccded10b1
Michael Duberstein <dubermic@flutterint.com>
2022-09-13 15:04:47 -0400

    In Clip 328 Automatically Handling Events, first use of the component prop

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -1,18 +1,21 @@
-import React from 'react'
-import {Field, reduxForm} from 'redux-form'
+import React from "react";
+import { Field, reduxForm } from "redux-form";
 
 class StreamCreate extends React.Component {
-  render(){
+  renderInput() {
+    return <div>I am an input!</div>;
+  }
+  render() {
     console.log("Props introduced with reduxForm", this.props);
     return (
       <form>
-        <Field name="title" />
-        <Field name="description" />
+        <Field name="title" component={this.renderInput} />
+        <Field name="description" component={this.renderInput} />
       </form>
     );
   }
 }
 
 export default reduxForm({
-  form: 'streamCreate'
+  form: "streamCreate",
 })(StreamCreate);
