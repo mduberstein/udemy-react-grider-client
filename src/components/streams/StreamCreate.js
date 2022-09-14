d4ebc69b2d02093e840e31db5ddc2d47590403a1
Michael Duberstein <dubermic@flutterint.com>
2022-09-13 22:16:46 -0400

    After Clip 334 Highlighting Errored Fields

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -15,8 +15,9 @@ class StreamCreate extends React.Component {
   renderInput = ({ input, label, meta }) => {
     // console.log("form input props:", input);
     console.log("meta ", meta);
+    const className = `field ${meta.error && meta.touched ? 'error': ''}`
     return (
-      <div className="field">
+      <div className={className}>
         <label>{label}</label>
         <input {...input} autoComplete="off" />
         {this.renderError(meta)}
@@ -38,7 +39,7 @@ class StreamCreate extends React.Component {
       // handleSubmit calls event.preventDefault(), so our onSubmit doesn't have to
       // also the onSubmit is called not with the event arguent,
       // but with values of the form's inputs
-      <form onSubmit={this.props.handleSubmit(this.onSubmit)} className="ui form">
+      <form onSubmit={this.props.handleSubmit(this.onSubmit)} className="ui form error">
         <Field name="title" component={this.renderInput} label="Enter Title" />
         <Field name="description" component={this.renderInput} label="Enter Description" />
         <button className="ui button primary">Submit</button>
