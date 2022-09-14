dae1be23419dffd79b14786e7b4005c835ea7afc
Michael Duberstein <dubermic@flutterint.com>
2022-09-13 22:02:59 -0400

    After Clip 333 Showing Errors on Touch

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -2,17 +2,27 @@ import React from "react";
 import { Field, reduxForm } from "redux-form";
 
 class StreamCreate extends React.Component {
-  renderInput({input, label, meta}) {
+  renderError({ error, touched }) {
+    if (touched && error) {
+      return (
+        <div className="ui error message">
+          <div className="header">{error}</div>
+        </div>
+      );
+    }
+  }
+
+  renderInput = ({ input, label, meta }) => {
     // console.log("form input props:", input);
     console.log("meta ", meta);
     return (
       <div className="field">
         <label>{label}</label>
-        <input {...input} />
-        <div>{meta.error}</div>
+        <input {...input} autoComplete="off" />
+        {this.renderError(meta)}
       </div>
     );
-  }
+  };
 
   // typical on Submit, before the Redux Form lib
   // onSubmit(event) {
@@ -26,11 +36,11 @@ class StreamCreate extends React.Component {
     // console.log("Props introduced with reduxForm", this.props);
     return (
       // handleSubmit calls event.preventDefault(), so our onSubmit doesn't have to
-      // also the onSubmit is called not with the event arguent, 
+      // also the onSubmit is called not with the event arguent,
       // but with values of the form's inputs
       <form onSubmit={this.props.handleSubmit(this.onSubmit)} className="ui form">
-        <Field name="title" component={this.renderInput} label="Enter Title"/>
-        <Field name="description" component={this.renderInput} label="Enter Description"/>
+        <Field name="title" component={this.renderInput} label="Enter Title" />
+        <Field name="description" component={this.renderInput} label="Enter Description" />
         <button className="ui button primary">Submit</button>
       </form>
     );
@@ -46,7 +56,7 @@ const validate = (formValues) => {
     errors.description = 'You must enter a description';
   }
   return errors;
-}
+};
 
 export default reduxForm({
   form: "streamCreate",
