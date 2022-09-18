d67f685e0d8e3f8f2a8157755d0c2415ca5a010a
Michael Duberstein <dubermic@flutterint.com>
2022-09-17 20:42:35 -0400

    In Clip Creating 339 Streams Through Action Creators, before npm install --save axios redux-thrunk --legacy-pear-depth

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -37,7 +37,7 @@ class StreamCreate extends React.Component {
     // console.log("Props introduced with reduxForm", this.props);
     return (
       // handleSubmit calls event.preventDefault(), so our onSubmit doesn't have to
-      // also the onSubmit is called not with the event arguent,
+      // also the onSubmit is called not with the event argument,
       // but with values of the form's inputs
       <form onSubmit={this.props.handleSubmit(this.onSubmit)} className="ui form error">
         <Field name="title" component={this.renderInput} label="Enter Title" />
