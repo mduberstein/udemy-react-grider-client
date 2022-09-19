c04f5c13702d2dfdcac93634481b01e3eba438f8
Michael Duberstein <dubermic@flutterint.com>
2022-09-19 17:24:12 -0400

    In Clip 350 Conditionally Showing Edit and Delete, conditionally display Edit/Delete placeholder

--- src/components/streams/StreamList.js
+++ src/components/streams/StreamList.js
@@ -7,6 +7,12 @@ class StreamList extends React.Component {
     this.props.fetchStreams();
   }
 
+  renderAdmin(stream) {
+    if(stream.userId === this.props.currentUserId) {
+      return <div>Edit/Delete</div>;
+    }
+  }
+
   renderList() {
     return this.props.streams.map(stream => {
       return (
@@ -16,6 +22,7 @@ class StreamList extends React.Component {
             {stream.title}
             <div className="description">{stream.description}</div>
           </div>
+          {this.renderAdmin(stream)}
         </div>
       );
     });
@@ -35,7 +42,10 @@ class StreamList extends React.Component {
 // Object.values returns array of values from a Javascript object
 // which is also a map, while Object.keys returns an array of keys
 const mapStateToProps = (state) => {
-  return { streams: Object.values(state.streams) };
+  return { 
+    streams: Object.values(state.streams),
+    currentUserId: state.auth.userId
+  };
 };
 
 export default connect(mapStateToProps, { fetchStreams })(StreamList);
