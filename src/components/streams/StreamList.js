9b9230eff7b3fa4f7fd54f04ab367c72e65b7377
Michael Duberstein <dubermic@flutterint.com>
2022-09-19 08:53:17 -0400

    In Clip 348 Rendering All Streams, after mapStateToProps

--- src/components/streams/StreamList.js
+++ src/components/streams/StreamList.js
@@ -6,9 +6,17 @@ class StreamList extends React.Component {
   componentDidMount() {
     this.props.fetchStreams();
   }
+
   render() {
+    console.log("props.streams: ", this.props.streams);
     return <div>StreamList</div>;
   }
 }
 
-export default connect(null, {fetchStreams})(StreamList);
+// Object.values returns array of values from a Javascript object
+// which is also a map, while Object.keys returns an array of keys
+const mapStateToProps = (state) => {
+  return { streams: Object.values(state.streams) };
+};
+
+export default connect(mapStateToProps, { fetchStreams })(StreamList);
