31d9e0ac9764b66ee79a3dd6a5d4209e14b6bb64
Michael Duberstein <dubermic@flutterint.com>
2022-09-18 20:45:32 -0400

    After Clip 344 Handling Fetching, Creating and Updating

---
+++ src/reducers/streamReducer.js
@@ -0,0 +1,18 @@
+import {
+  FETCH_STREAM, FETCH_STREAMS, CREATE_STREAM, EDIT_STREAM, DELETE_STREAM
+} from "../actions/types";
+
+const streamReducer = (state = {}, action) => {
+  switch(action.type){
+    case FETCH_STREAM:
+      return {...state, [action.payload.id]: action.payload};
+    case CREATE_STREAM:
+      return {...state, [action.payload.id]: action.payload};
+    case EDIT_STREAM:
+      return {...state, [action.payload.id]: action.payload};
+    default:
+      return state;
+  }
+};
+
+export default streamReducer;
