142efceac613f2d6226d112535ee5aa87a7a3c9b
Michael Duberstein <dubermic@flutterint.com>
2022-09-19 21:45:08 -0400

    After Clip 361 Selecting Records from State

--- src/components/streams/StreamEdit.js
+++ src/components/streams/StreamEdit.js
@@ -1,8 +1,15 @@
-import React from 'react'
+import React from 'react';
+import {connect} from 'react-redux';
 
 const StreamEdit = (props) => {
-  console.log("SreamEdit props: ", props)
+  console.log("SreamEdit props: ", props) 
   return <div>StreamEdit</div>;
-}
+};
 
-export default StreamEdit;
+const mapStateToProps = (state, ownProps) => {
+  console.log("In SreamEdit mapStateToProp, ownProps", ownProps);
+  return {
+    stream: state.streams[ownProps.match.params.id]
+  };
+};
+export default connect(mapStateToProps)(StreamEdit);
