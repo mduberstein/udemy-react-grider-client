20543bb4f022b9e7d174645a3238f8fb711542cb
Michael Duberstein <dubermic@flutterint.com>
2022-09-20 09:28:08 -0400

    After Clip 363 Fetching a Stream for Edit Stream

--- src/components/streams/StreamEdit.js
+++ src/components/streams/StreamEdit.js
@@ -1,15 +1,28 @@
 import React from 'react';
 import {connect} from 'react-redux';
+import {fetchStream} from '../../actions/index';
 
-const StreamEdit = (props) => {
-  console.log("SreamEdit props: ", props) 
-  return <div>StreamEdit</div>;
-};
+// const StreamEdit = (props) => {
+//   console.log("SreamEdit props: ", props) 
+//   return <div>StreamEdit</div>;
+// };
+class StreamEdit  extends React.Component {
+  componentDidMount() {
+    this.props.fetchStream(this.props.match.params.id);
+  }
+  render () {
+    console.log("SreamEdit props: ", this.props);
+    if(!this.props.stream) {
+      return <div>Loading...</div>;
+    }
+    return <div>{this.props.stream.title}</div>;
+  };
+}
 
 const mapStateToProps = (state, ownProps) => {
-  console.log("In SreamEdit mapStateToProp, ownProps", ownProps);
+  // console.log("In SreamEdit mapStateToProp, ownProps", ownProps);
   return {
     stream: state.streams[ownProps.match.params.id]
   };
 };
-export default connect(mapStateToProps)(StreamEdit);
+export default connect(mapStateToProps, {fetchStream})(StreamEdit);
