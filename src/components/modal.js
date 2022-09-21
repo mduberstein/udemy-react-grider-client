0a936a9324e4f45b8780cd9a234b7aeff855584f
Michael Duberstein <dubermic@flutterint.com>
2022-09-21 12:10:19 -0400

    In Clip 373 Hiding a Modal, before implementing hiding

--- src/components/modal.js
+++ src/components/modal.js
@@ -5,7 +5,14 @@ const Modal = (props) => {
   return ReactDOM.createPortal(
     // JSX for the modal itself
     <div className="ui dimmer modals visible active">
-      <div className="ui standard modal visible active">Some Modal</div>
+      <div className="ui standard modal visible active">
+        <div className="header">Delete Stream</div>
+        <div className="content">Are you sure you want to delete this stream?</div>
+        <div className="actions">
+          <div className="ui primary button">Delete</div>
+          <div className="ui button">Cancel</div>
+        </div>
+      </div>
     </div>, 
     // JSX for the parent
     document.querySelector("#modal")
