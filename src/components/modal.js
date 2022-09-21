2e7567659d69ecc4bfebe46ea22b899398829d1a
Michael Duberstein <dubermic@flutterint.com>
2022-09-21 13:07:58 -0400

    After Clip 374 Making the Modal Reusable

--- src/components/modal.js
+++ src/components/modal.js
@@ -10,19 +10,14 @@ const Modal = (props) => {
       className="ui dimmer modals visible active"
     >
       <div
-      // prevents the click just outside the buttons 
-      // from bubbling up to parent  and closing the Modal
+        // prevents the click just outside the buttons
+        // from bubbling up to parent  and closing the Modal
         onClick={(e) => e.stopPropagation()}
         className="ui standard modal visible active"
       >
-        <div className="header">Delete Stream</div>
-        <div className="content">
-          Are you sure you want to delete this stream?
-        </div>
-        <div className="actions">
-          <div className="ui primary button">Delete</div>
-          <div className="ui button">Cancel</div>
-        </div>
+        <div className="header">{props.title}</div>
+        <div className="content">{props.content}</div>
+        <div className="actions">{props.actions}</div>
       </div>
     </div>,
     // JSX for the parent
--- src/components/streams/StreamDelete.js
+++ src/components/streams/StreamDelete.js
@@ -1,12 +1,22 @@
-import React from 'react'
-import Modal from '../modal'
+import React from "react";
+import Modal from "../modal";
 const StreamDelete = () => {
+  const actions = (
+    <div>
+      <div className="ui button negative">Delete</div>
+      <div className="ui button">Cancel</div>
+    </div> 
+  );
   return (
     <div>
       StreamDelete
-      <Modal />
+      <Modal
+        title="Delete Stream"
+        content="Are you sure you want to delete this stream?"
+        actions={actions}
+      />
     </div>
   );
-}
+};
 
 export default StreamDelete;
